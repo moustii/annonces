2b3277d3bf121c3f71d60241e847b073eb5056c0
moustii <jeremy.ibert@outlook.com>
2021-01-31 18:24:45 +0100

    add: new files and update

---
+++ view/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>on Smell | Home</title>
+        <!-- css link -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css" type="text/css">
+    </head>
+    <body>
+
+        <!--HEADER  -->
+        <header class="container-fluid">
+            <!-- logo -->
+            <h1>on sMell</h1>
+
+            <!-- devise -->
+            <p>Find all you want here</p>
+        </header>
+        <!-- END HEADER -->
+
+        <!-- SECTION ADS -->
+        <section class="container-fluid ads">
+            <div class="row justify-content-around space">
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row justify-content-around ">
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+                <div class="card col-sm-3 col-md-4 col-lg-3">
+                    <img class="card-img-top" src="image/perfume.jpg" alt="ad image or nothing">
+                    <div class="card-body">
+                        <h4 class="card-title">Bleu de channel</h4>
+                        <p class="card-text">75.00 $</p>
+                        <a href="#" class="btn text-right float-right">Show more</a>
+                    </div>
+                </div>
+
+            </div>
+            
+        </section>
+
+
+
+        
+        <!-- SECTION BUTTON -->
+        <section class="add">
+            <button>Add an ad</button>
+        </section>
+
+
+
+
+
+
+
+
+
+        <!-- FOOTER -->
+        <footer>
+            <span>2021 &copy; on sMell</span>
+        </footer>
+
+        <!-- js link -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    </body>
+</html>
